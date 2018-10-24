5e8052b59c6db0b06c329a0a40fe3dfd22083f67
BT_1N3_22 <dwong0023@bths.edu>
2018-10-24 13:12:33 -0400

    changer

---
+++ src/com/company/Horse.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface Horse {
+    private String name;
+    private int weight;
+
+
+}
---
+++ src/com/company/Mustang.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Mustang implements Horse{
+    public String name;
+    public int weight;
+
+    public Mustang(String name, int weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public static String getName()
+    {
+        return this.name;
+    }
+
+    public static String getWeight()
+    {
+        return this.weight;
+    }
+}
